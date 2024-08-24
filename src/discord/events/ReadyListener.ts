2d139b44189bb6707ff344298c5ed8db47ef23d7
Davi GC <42722614+DaviGCD@users.noreply.github.com>
2024-08-24 02:23:44 -0300

    Remove the change banner system

--- src/discord/events/ReadyListener.ts
+++ src/discord/events/ReadyListener.ts
@@ -3,15 +3,6 @@ import { ActivityPartial } from 'eris'
 import { Logger } from '../../Logger'
 import { ChinoClient } from '../ChinoClient.platform'
 import { Config } from '../config'
-const banner = [
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192861365764126/Screenshot_20200731-202613_Goyabu.jpg',
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192867678060676/Screenshot_20200731-202639_Goyabu.jpg',
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192873562669166/Screenshot_20200731-202743_Goyabu.jpg',
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192879334162452/Screenshot_20200731-202804_Goyabu.jpg',
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192885956706394/Screenshot_20200731-210716_Goyabu.jpg',
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192889421463622/Screenshot_20200801-233101_Goyabu.jpg',
-  'https://cdn.discordapp.com/attachments/481807707066859530/751192907985322005/Screenshot_20200802-003003_Goyabu.jpg'
-]
 const status: ActivityPartial[] = [
   { name: 'Trying to give you support.', type: 3 },
   { name: 'Moderating my support server!', type: 3 },
@@ -29,13 +20,6 @@ export default {
 
     try {
       Promise.all([setInterval(async () => {
-        if (!guild?.features?.includes('BANNER')) return
-        const buffer = await axios.get(banner[Math.floor(Math.random() * banner.length)], { responseType: 'arraybuffer' }).then(d => Buffer.from(d.data, 'binary').toString('base64'))
-        const base64Banner = `data:image/${banner[Math.floor(Math.random() * banner.length)].substr(banner[Math.floor(Math.random() * banner.length)].length - 3)};base64,${buffer}`
-        guild.edit({
-          banner: base64Banner
-        })
-      }, 900000),
       setInterval(() => {
         const game = status[Math.round(Math.random() * status.length)]
         if (game?.type === 0) {
